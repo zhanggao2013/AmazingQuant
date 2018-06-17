f406d0b3797f3cde1564901bf4171ab76bdd70b7
zhanggao2013 <82045571@qq.com>
2018-06-17 22:45:34 +0800

    id全大写

--- AmazingQuant/utils/generate_random_id.py
+++ AmazingQuant/utils/generate_random_id.py
@@ -4,12 +4,14 @@ __author__ = "gao"
 
 import random
 
+from AmazingQuant.constant import ID
 
-def generate_random_id(topic="", lens=8):
-    _list = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]
+
+def generate_random_id(topic=ID.ACCOUNT_ID.value, lens=8):
+    _list = [chr(i) for i in range(65, 91)] + [str(i) for i in range(10)]
     num = random.sample(_list, lens)
     return "{}_{}".format(topic, "".join(num))
 
 
 if __name__ == "__main__":
-    print(generate_random_id(topic="aa", lens=8))
+    print(generate_random_id(topic=ID.ORDER_ID.value))
