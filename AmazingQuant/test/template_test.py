a9d0357341e900c593a8a22c80f1bc58b7d985e4
gao <82045571@qq.com>
2019-11-14 03:57:19 +0800

    模板设置

---
+++ AmazingQuant/test/template_test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : ${DATE}
+# @Author  : gao
+# @File    : ${NAME}.py
+# @Project : ${PROJECT_NAME}
+# ------------------------------
