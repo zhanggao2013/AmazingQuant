fea1e2cc0f6cd12dbdd8f6abfeff3aee8285682e
gao <82045571@qq.com>
2019-11-13 21:42:42 +0800

    修改模板

--- example/test_strategy.py
+++ example/test_strategy.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
-__author__ = "gao"
+# ------------------------------
+# @Time    : 2019/11/14
+# @Author  : gao
+# @File    : test_strategy.py.py
+# @Project : AmazingQuant
+# ------------------------------
 
 import numpy as np
 import talib
