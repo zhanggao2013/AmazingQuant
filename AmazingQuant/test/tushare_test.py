504e0c6e5488a062a393c74bd7c95580928ac110
gao <82045571@qq.com>
2019-11-13 21:39:07 +0800

    修改模板

--- AmazingQuant/test/tushare_test.py
+++ AmazingQuant/test/tushare_test.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 
-__author__ = "gao"
-
+# ------------------------------
+# @Time    : 2019/11/14
+# @Author  : gao
+# @File    : tushare_test.py.py
+# @Project : AmazingQuant
+# ------------------------------
