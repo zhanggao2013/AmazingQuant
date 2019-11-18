b6f5aa7601229b8f627cc87f7567ce2c25267859
gao <82045571@qq.com>
2019-11-18 19:22:42 +0800

    data DictField改为 ListField(required=True)

---
+++ AmazingQuant/data_center/save_data/save_finance_data.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : 2019/11/18
+# @Author  : gao
+# @File    : save_finance_data.py
+# @Project : AmazingQuant 
+# ------------------------------
