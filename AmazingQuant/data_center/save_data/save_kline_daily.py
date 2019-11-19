b17446717e1b0a7c87c71b2bb6f952369e7dae0b
zhanggao2013 <82045571@qq.com>
2019-11-19 18:05:54 +0800

    存日K线，改为一只股票存一个collection

--- AmazingQuant/data_center/save_data/save_kline_daily.py
+++ AmazingQuant/data_center/save_data/save_kline_daily.py
@@ -13,6 +13,7 @@ from multiprocessing import Pool
 import pandas as pd
 import numpy as np
 from mongoengine import Document
+from mongoengine.context_managers import switch_collection
 from mongoengine.fields import DictField, ListField, StringField, FloatField, IntField, DateTimeField
 from AmazingQuant.data_center.mongo_connection import MongoConnect
 from AmazingQuant.utils.performance_test import Timer
@@ -22,9 +23,8 @@ class KlineDaily(Document):
     update_date = DateTimeField(default=datetime.utcnow())
     security_code = StringField(required=True)
     date = IntField(required=True)
-    time_tag = IntField(required=True)
     data = ListField(required=True)
-    meta = {'indexes': ['security_code', 'time_tag'], 'shard_key': ('security_code', 'time_tag',)}
+    meta = {'indexes': ['security_code', 'date'], 'shard_key': ('security_code', 'date',)}
 
 
 class SaveKlineDaily(object):
@@ -44,6 +44,8 @@ class SaveKlineDaily(object):
                 print('完成数量：', file_num)
                 p.apply_async(self.insert_security_code, args=(market, file_name, path))
                 # self.insert_security_code(market, file_name, path)
+            p.close()
+            p.join()
 
     def insert_security_code(self, market, file_name, path):
         database = 'kline'
@@ -52,22 +54,22 @@ class SaveKlineDaily(object):
             kline_daily_data = pd.read_csv(path + file_name, encoding='unicode_escape')
             date = kline_daily_data['date'][0]
             if not np.isnan(date):
-                time_tag = kline_daily_data['time'][0]
                 security_code = file_name.split('.')[0] + '.' + market
                 kline_daily_data = kline_daily_data.reindex(columns=['open', 'high', 'low', 'close', 'volumw',
                                                                      'turover', 'match_items', 'interest'])
                 kline_daily_data.rename(columns={'volumw': 'volume', 'turover': 'amount'},  inplace=True)
-                doc_list = []
-                for index, row in kline_daily_data.iterrows():
-                    data = [int(value) for value in dict(row).values()]
-                    data = [int(row['open']), int(row['high']), int(row['low']), int(row['close']), int(row['volume']),
-                            int(row['amount']), int(row['match_items']), int(row['interest']), ]
-                    doc = KlineDaily(security_code=security_code,
-                                     date=int(date),
-                                     time_tag=int(time_tag),
-                                     data=data)
-                    doc_list.append(doc)
-                KlineDaily.objects.insert(doc_list)
+                with switch_collection(KlineDaily, security_code) as KlineDaily_security_code:
+                    doc_list = []
+                    for index, row in kline_daily_data.iterrows():
+                        data = [int(value) for value in dict(row).values()]
+                        data = [int(row['open']), int(row['high']), int(row['low']), int(row['close']), int(row['volume']),
+                                int(row['amount']), int(row['match_items']), int(row['interest']), ]
+                        doc = KlineDaily_security_code(security_code=security_code,
+                                                       date=int(date),
+                                                       data=data)
+                        doc_list.append(doc)
+
+                    KlineDaily_security_code.objects.insert(doc_list)
 
 
 if __name__ == '__main__':
