7fd1413952b4d5e363c38fd472be7ddc915915d0
gao <82045571@qq.com>
2019-11-23 22:50:09 +0800

    复权因子更新到除权除息表的adj_factor字段

--- AmazingQuant/data_center/database_field/field_a_share_ex_right_dividend.py
+++ AmazingQuant/data_center/database_field/field_a_share_ex_right_dividend.py
@@ -43,5 +43,7 @@ class AShareExRightDividend(Document):
     seo_ratio = FloatField(required=True, null=True)
     # 缩减比例
     consolidate_split_ratio = FloatField(required=True, null=True)
+    # 复权因子
+    adj_factor = FloatField()
 
     meta = {'indexes': ['security_code', 'ex_date']}
--- AmazingQuant/data_center/save_data/save_a_share_adj_factor.py
+++ AmazingQuant/data_center/save_data/save_a_share_adj_factor.py
@@ -11,8 +11,7 @@ import pandas as pd
 import numpy as np
 
 from AmazingQuant.data_center.database_field.field_a_share_ex_right_dividend import AShareExRightDividend
-from AmazingQuant.data_center.database_field.field_a_share_adj_factor import AShareAdjFactor
-from AmazingQuant.data_center.database_field.f
+from AmazingQuant.data_center.database_field.field_a_share_kline import Kline
 from AmazingQuant.data_center.mongo_connection import MongoConnect
 from AmazingQuant.utils.transfer_field import get_field_str_list
 
@@ -23,38 +22,16 @@ class SaveAShareAdjFactor(object):
         self.field_is_str_list = get_field_str_list(field_path)
 
     def save_a_share_adj_factor(self):
+        """
+        取当日收盘价，作为转、送的股价，
+        再计算复权因子更新到AShareExRightDividend, 复权因子adj_factor
+        :return:
+        """
         database = 'stock_base_data'
         with MongoConnect(database):
-            doc_list = []
-            for index, row in self.data_df.iterrows():
-                row_dict = dict(row)
-
-                row_dict['security_code'] = row_dict['S_INFO_WINDCODE']
-                row_dict.pop('OBJECT_ID')
-                row_dict.pop('S_INFO_WINDCODE')
-
-                doc = AShareExRightDividend()
-                for key, value in row_dict.items():
-                    if key.lower() in self.field_is_str_list:
-                        if key.lower() in ['ex_date']:
-                            if np.isnan(value):
-                                setattr(doc, key.lower(), None)
-                            else:
-                                setattr(doc, key.lower(), str(int(value)))
-                        else:
-                            setattr(doc, key.lower(), str(value))
-                    else:
-                        setattr(doc, key.lower(), value)
-                doc_list.append(doc)
-                if len(doc_list) > 999:
-                    AShareExRightDividend.objects.insert(doc_list)
-                    doc_list = []
-            else:
-                AShareExRightDividend.objects.insert(doc_list)
+            pass
 
 
 if __name__ == '__main__':
-    data_path = '../../../../data/finance/AShareEXRightDividendRecord.csv'
-    field_path = '../../config/field_a_share_ex_right_dividend.txt'
     save_cash_flow_obj = SaveAShareAdjFactor(data_path, field_path)
     save_cash_flow_obj.save_a_share_adj_factor()
