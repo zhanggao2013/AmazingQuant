65b6fc01eb456517b7ccbe4df363c0602b038c98
gao <82045571@qq.com>
2019-12-11 07:12:01 +0800

    security_code_data = security_code_data.fillna(0),

--- AmazingQuant/data_center/save_data/save_kline_daily.py
+++ AmazingQuant/data_center/save_data/save_kline_daily.py
@@ -8,12 +8,11 @@
 
 from datetime import datetime
 import os
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
 
 import pandas as pd
 import numpy as np
 from mongoengine.context_managers import switch_collection
-from multiprocessing import Pool, Manager
 
 from AmazingQuant.data_center.mongo_connection import MongoConnect
 from AmazingQuant.utils.performance_test import Timer
@@ -58,7 +57,7 @@ class SaveKlineDaily(object):
                     security_code_data = pd.DataFrame()
                     if security_code in self.data_dict.keys():
                         security_code_data = self.data_dict[security_code].set_index(["TRADE_DT"])
-
+                        security_code_data = security_code_data.fillna(0)
                     for index, row in kline_daily_data.iterrows():
                         date_int = int(row['date'])
                         if not np.isnan(date_int):
@@ -78,24 +77,22 @@ class SaveKlineDaily(object):
                     # 用csv全表补充20020104之前的日线数据，match_items为0
                     security_code_data = security_code_data[security_code_data.index < 20020104]
                     for index, row in security_code_data.iterrows():
-                        # 排除成交量为0的，停牌
-                        if row['S_DQ_AMOUNT'] > 0:
-                            try:
-                                pre_close = int(row['S_DQ_PRECLOSE'] * 10000)
-                            except KeyError:
-                                pre_close = None
-                            date_int = int(index)
-                            date_int = str(date_int)
-                            time_tag = datetime.strptime(date_int, "%Y%m%d")
-                            doc = KlineDaily_security_code(time_tag=time_tag, pre_close=pre_close,
-                                                           open=int(row['S_DQ_OPEN'] * 10000),
-                                                           high=int(row['S_DQ_HIGH'] * 10000),
-                                                           low=int(row['S_DQ_LOW'] * 10000),
-                                                           close=int(row['S_DQ_CLOSE'] * 10000),
-                                                           volume=int(row['S_DQ_VOLUME'] * 100),
-                                                           amount=int(row['S_DQ_AMOUNT'] * 1000),
-                                                           match_items=0, interest=0)
-                            doc_list.append(doc)
+                        date_int = int(index)
+                        date_int = str(date_int)
+                        time_tag = datetime.strptime(date_int, "%Y%m%d")
+                        try:
+                            pre_close = int(row['S_DQ_PRECLOSE'] * 10000)
+                        except KeyError:
+                            pre_close = None
+                        doc = KlineDaily_security_code(time_tag=time_tag, pre_close=pre_close,
+                                                       open=int(row['S_DQ_OPEN'] * 10000),
+                                                       high=int(row['S_DQ_HIGH'] * 10000),
+                                                       low=int(row['S_DQ_LOW'] * 10000),
+                                                       close=int(row['S_DQ_CLOSE'] * 10000),
+                                                       volume=int(row['S_DQ_VOLUME'] * 100),
+                                                       amount=int(row['S_DQ_AMOUNT'] * 1000),
+                                                       match_items=0, interest=0)
+                        doc_list.append(doc)
                     KlineDaily_security_code.objects.insert(doc_list)
 
 
