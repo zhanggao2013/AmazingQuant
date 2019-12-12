df91c66a13c5a54a447bfe25353dd6ec53f88940
gao <82045571@qq.com>
2019-12-13 06:47:13 +0800

    取历史板块成分股

---
+++ AmazingQuant/data_center/get_data/get_index_member.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : 2019/12/13
+# @Author  : gao
+# @File    : get_index_member.py
+# @Project : AmazingQuant 
+# ------------------------------
