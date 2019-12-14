306975d8bbef27477eeda517da105a12be71ab68
gao <82045571@qq.com>
2019-12-14 22:06:20 +0800

    所有取数据都单独连接ｍｏｎｇｏ

--- AmazingQuant/data_center/get_data/get_calendar.py
+++ AmazingQuant/data_center/get_data/get_calendar.py
@@ -8,23 +8,25 @@
 # ------------------------------
 
 from datetime import datetime
+from mongoengine import connection
 
-from AmazingQuant.data_center.mongo_connection import MongoConnect
+from AmazingQuant.config.database_info import MongodbConfig
 from AmazingQuant.data_center.database_field.filed_a_share_calendar import AShareCalendar
 from AmazingQuant.constant import DatabaseName
 
 
 class GetCalendar(object):
     def __init__(self):
-        pass
+        self.database = DatabaseName.STOCK_BASE_DATA.value
 
     def get_calendar(self, market):
-        database = DatabaseName.STOCK_BASE_DATA.value
-        with MongoConnect(database):
-            data = AShareCalendar.objects(market=market).as_pymongo()
-            trade_days = data[0]['trade_days']
-            trade_days = [i for i in trade_days if i < datetime.now()]
-            return trade_days
+        connection.connect(db=self.database, host=MongodbConfig.host, port=MongodbConfig.port,
+                           password=MongodbConfig.password, username=MongodbConfig.username, retryWrites=False)
+        data = AShareCalendar.objects(market=market).as_pymongo()
+        trade_days = data[0]['trade_days']
+        trade_days = [i for i in trade_days if i < datetime.now()]
+        connection.disconnect()
+        return trade_days
 
 
 if __name__ == '__main__':
