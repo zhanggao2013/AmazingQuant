54223bc9d01e550a6be2acaebffe288c6ccb1f13
zhanggao2013 <82045571@qq.com>
2019-12-31 13:34:35 +0800

    format='table'报错，不用了，默认fixed

--- AmazingQuant/data_center/update_local_data/save_data.py
+++ AmazingQuant/data_center/update_local_data/save_data.py
@@ -12,6 +12,6 @@ import os
 def save_data_to_hdf5(path, data_name, input_data, is_append=False):
     if not os.path.exists(path):
         os.mkdir(path)
-    input_data.to_hdf(path + data_name + '.h5', key=data_name, mode='w', format='table', append=is_append)
+    input_data.to_hdf(path + data_name + '.h5', key=data_name, mode='w', append=is_append)
 
 
