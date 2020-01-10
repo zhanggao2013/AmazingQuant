f61213da4aba6379623a32a37947221ece1bea3b
zhanggao2013 <82045571@qq.com>
2020-01-10 16:40:36 +0800

    取复权因子api

---
+++ AmazingQuant/data_center/api_data/get_adj_factor.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : 2020/1/10
+# @Author  : gao
+# @File    : get_adj_factor.py
+# @Project : AmazingQuant
+# ------------------------------
