cfa06dbcc2dfd14c40383091604c9fd40a7fe418
zhanggao2013 <82045571@qq.com>
2020-01-10 18:04:17 +0800

    get_adj_factor增加AdjustmentMode参数

--- AmazingQuant/data_center/api_data/get_adj_factor.py
+++ AmazingQuant/data_center/api_data/get_adj_factor.py
@@ -7,50 +7,29 @@
 # @Project : AmazingQuant
 # ------------------------------
 
-from datetime import datetime
-
 import pandas as pd
-
-from AmazingQuant.constant import LocalDataFolderName
+from AmazingQuant.constant import LocalDataFolderName, AdjustmentMode
 from AmazingQuant.config.local_data_path import LocalDataPath
 
 
-class GetIndexMember(object):
+class GetAdjFactor(object):
     def __init__(self):
-        self.all_index_members_df = pd.DataFrame.empty
-        self.index_members_df = pd.DataFrame.empty
-        self.index_members_all = []
+        pass
 
-    def get_all_index_members(self):
-        folder_name = LocalDataFolderName.INDEX_MEMBER.value
+    def get_adj_factor(self, adjustment_mode):
+        folder_name = LocalDataFolderName.ADJ_FACTOR.value
         path = LocalDataPath.path + folder_name + '/'
-        data_name = folder_name + '.h5'
-        self.all_index_members_df = pd.read_hdf(path + data_name)
-        return self.all_index_members_df
-
-    def get_index_members(self, index_code):
-        self.index_members_df = self.all_index_members_df[self.all_index_members_df.index_code == index_code]
-        self.index_members_df = self.index_members_df.fillna(datetime.now()).reset_index(drop=True)
-        self.index_members_all = list(set(self.index_members_df['security_code']))
-        return self.index_members_df, self.index_members_all
-
-    def get_index_member_in_date(self, members_date=datetime.now()):
-        """
-        取指定日期的指数成分股
-        :param members_date:
-        :return:
-        """
-        index_members_in_date_df = self.index_members_df[
-            (self.index_members_df.in_date <= members_date) & (self.index_members_df.out_date >= members_date)]
-        return list(index_members_in_date_df["security_code"])
+        data = pd.DataFrame({})
+        if adjustment_mode == AdjustmentMode.RIGHT.value:
+            data_name = folder_name + '_' + adjustment_mode + '.h5'
+            data = pd.read_hdf(path + data_name)
+        elif adjustment_mode == AdjustmentMode.RIGHT.value:
+            data_name = folder_name + '_' + adjustment_mode + '.h5'
+            data = pd.read_hdf(path + data_name)
+        return data
 
 
 if __name__ == '__main__':
-    index_member_obj = GetIndexMember()
-    all_index_members_df = index_member_obj.get_all_index_members()
-    # 深证综指
-    index_members_df_SZ, index_members_all_SZ = index_member_obj.get_index_members('399106.SZ')
-    # 上证Ａ股
-    index_members_df_SH, index_members_all_SH = index_member_obj.get_index_members('000001.SH')
-    print()
-    index_member_in_date = index_member_obj.get_index_member_in_date()
+    adj_factor_obj = GetAdjFactor()
+    result = adj_factor_obj.get_adj_factor(AdjustmentMode.RIGHT.value)
+    print(result)
