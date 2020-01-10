f128063e58685b3d0c8cc967ba5973a6529dd9d1
zhanggao2013 <82045571@qq.com>
2020-01-10 16:56:46 +0800

    改为正常的连接mongo形式

--- AmazingQuant/data_center/update_local_data/update_index_member.py
+++ AmazingQuant/data_center/update_local_data/update_index_member.py
@@ -7,16 +7,13 @@
 # @Project : AmazingQuant 
 # ------------------------------
 
-from datetime import datetime
-
 import pandas as pd
-from mongoengine import connection
 
 from AmazingQuant.constant import DatabaseName, LocalDataFolderName
-from AmazingQuant.config.database_info import MongodbConfig
 from AmazingQuant.config.local_data_path import LocalDataPath
 from AmazingQuant.data_center.database_field.field_a_share_index_members import AShareIndexMembers
 from AmazingQuant.data_center.update_local_data.save_data import save_data_to_hdf5
+from AmazingQuant.data_center.mongo_connection_me import MongoConnect
 
 
 class UpdateGIndexMember(object):
@@ -26,18 +23,14 @@ class UpdateGIndexMember(object):
 
     def update_index_members(self):
         database = DatabaseName.STOCK_BASE_DATA.value
-        connection.connect(db=database, host=MongodbConfig.host, port=MongodbConfig.port,
-                           password=MongodbConfig.password, username=MongodbConfig.username, retryWrites=False)
-        index_members_data = AShareIndexMembers.objects().as_pymongo()
-        connection.disconnect()
-
-        field_list = ['index_code', 'security_code', 'in_date', 'out_date']
-        self.index_members_df = pd.DataFrame(list(index_members_data)).reindex(columns=field_list)
-        print(self.index_members_df)
-        folder_name = LocalDataFolderName.INDEX_MEMBER.value
-        path = LocalDataPath.path + folder_name + '/'
-        data_name = folder_name
-        save_data_to_hdf5(path, data_name, self.index_members_df)
+        with MongoConnect(database):
+            index_members_data = AShareIndexMembers.objects().as_pymongo()
+            field_list = ['index_code', 'security_code', 'in_date', 'out_date']
+            self.index_members_df = pd.DataFrame(list(index_members_data)).reindex(columns=field_list)
+            folder_name = LocalDataFolderName.INDEX_MEMBER.value
+            path = LocalDataPath.path + folder_name + '/'
+            data_name = folder_name
+            save_data_to_hdf5(path, data_name, self.index_members_df)
 
 
 if __name__ == '__main__':
