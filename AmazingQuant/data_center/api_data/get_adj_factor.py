775d554ad3f6f7df117c0f203451bd7112dfa03e
gao <82045571@qq.com>
2020-01-11 23:45:51 +0800

    return dict

--- AmazingQuant/data_center/api_data/get_adj_factor.py
+++ AmazingQuant/data_center/api_data/get_adj_factor.py
@@ -8,8 +8,10 @@
 # ------------------------------
 
 import pandas as pd
+
 from AmazingQuant.constant import LocalDataFolderName, AdjustmentMode
 from AmazingQuant.config.local_data_path import LocalDataPath
+from AmazingQuant.utils.performance_test import Timer
 
 
 class GetAdjFactor(object):
@@ -26,10 +28,14 @@ class GetAdjFactor(object):
         elif adjustment_mode == AdjustmentMode.RIGHT.value:
             data_name = folder_name + '_' + adjustment_mode + '.h5'
             data = pd.read_hdf(path + data_name)
-        return data
+        return dict(list(data.groupby(data['security_code'])))
 
 
 if __name__ == '__main__':
-    adj_factor_obj = GetAdjFactor()
-    result = adj_factor_obj.get_adj_factor(AdjustmentMode.RIGHT.value)
-    print(result)
+    with Timer(True):
+        adj_factor_obj = GetAdjFactor()
+        result = adj_factor_obj.get_adj_factor(AdjustmentMode.RIGHT.value)
+        # print(result)
+"""
+存成与日线数据相同格式的hdf5文件，取出来直接与open   close相乘
+"""
