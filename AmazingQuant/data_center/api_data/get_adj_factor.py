8a6be342975ad54b49b3861c39f8312b16536f4a
gao <82045571@qq.com>
2020-01-12 23:13:50 +0800

    删除无效代码

--- AmazingQuant/data_center/api_data/get_adj_factor.py
+++ AmazingQuant/data_center/api_data/get_adj_factor.py
@@ -23,22 +23,16 @@ class GetAdjFactor(object):
         path = LocalDataPath.path + folder_name + '/'
         data = pd.DataFrame({})
         if adjustment_mode == AdjustmentMode.RIGHT.value:
-            data_name = folder_name + '_' + adjustment_mode + '.h5'
+            data_name = adjustment_mode + '.h5'
             data = pd.read_hdf(path + data_name)
         elif adjustment_mode == AdjustmentMode.RIGHT.value:
-            data_name = folder_name + '_' + adjustment_mode + '.h5'
+            data_name = adjustment_mode + '.h5'
             data = pd.read_hdf(path + data_name)
         return data
 
+
 if __name__ == '__main__':
-    from AmazingQuant.data_center.api_data.get_calender import GetCalendar
     with Timer(True):
         adj_factor_obj = GetAdjFactor()
         result = adj_factor_obj.get_adj_factor(AdjustmentMode.RIGHT.value)
-        # result_a = {}
-        # for i in result:
-        #     result[i].set_index(["ex_date"], inplace=True)
-        # print(result)
-"""
-存成与日线数据相同格式的hdf5文件，取出来直接与open   close相乘
-"""
+
