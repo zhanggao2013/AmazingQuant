0bb1fcf321afee2c2564b2effc61ca55de21f91a
gao <37475036@qq.com>
2020-02-10 15:14:33 +0800

    增加模板

---
+++ documents/template.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : 2020/2/10
+# @Author  : gao
+# @File    : template.py
+# @Project : AmazingQuant
+# ------------------------------
+
+
+
+
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : ${DATE}
+# @Author  : gao
+# @File    : ${NAME}.py
+# @Project : ${PROJECT_NAME}
+# ------------------------------
