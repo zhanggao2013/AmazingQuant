a986253857979e22d5317c57df8624ce4df11bcb
gao <37475036@qq.com>
2020-03-31 23:23:43 +0800

    多因子框架

---
+++ AmazingQuant/multi_factor/combinatorial_optimazation/factor_return_estimation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : 2020/3/31
+# @Author  : gao
+# @File    : factor_return_estimation.py
+# @Project : AmazingQuant
+# ------------------------------
+"""
+
+"""
---
+++ AmazingQuant/multi_factor/combinatorial_optimazation/specific_factor_vol_adjust.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : 2020/3/31
+# @Author  : gao
+# @File    : specific_factor_vol_adjust.py
+# @Project : AmazingQuant
+# ------------------------------
+"""
+特质因子收益率方差矩阵调整
+（1）Newey-West 调整
+（2）结构化调整
+（3）贝叶斯压缩调整
+（4）波动率偏误调整
+"""
