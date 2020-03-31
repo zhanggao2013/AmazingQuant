6d9a326bac2c08a93db6b45fd5d114594784d574
gao <37475036@qq.com>
2020-03-31 21:20:25 +0800

    统一默认路径

--- AmazingQuant/indicator_center/save_get_indicator.py
+++ AmazingQuant/indicator_center/save_get_indicator.py
@@ -16,16 +16,15 @@ from AmazingQuant.config.local_data_path import LocalDataPath
 
 class SaveGetIndicator(object):
     def __init__(self):
-        pass
+        self.path_save = LocalDataPath.path + 'indicator/'
 
     def save_indicator(self, indicator_name, input_data):
-        path_save = LocalDataPath.path + 'indicator/'
-        if not os.path.exists(path_save):
-            os.mkdir(path_save)
-        input_data.to_hdf(path_save + indicator_name + '.h5', key=indicator_name, mode='w')
+        if not os.path.exists(self.path_save):
+            os.mkdir(self.path_save)
+        input_data.to_hdf(self.path_save + indicator_name + '.h5', key=indicator_name, mode='w')
 
     def get_indicator(self, indicator_name):
-        data_path = LocalDataPath.path + 'indicator/' + indicator_name + '.h5'
+        data_path = self.path_save + indicator_name + '.h5'
         if not os.path.exists(data_path):
             return None
         return pd.read_hdf(data_path)
