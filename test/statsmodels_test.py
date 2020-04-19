99b6e413e4f4c8f7f509363245c14f037dc06fb8
gao <37475036@qq.com>
2020-04-19 22:47:06 +0800

    不需要加一列1

--- test/statsmodels_test.py
+++ test/statsmodels_test.py
@@ -12,7 +12,6 @@ import statsmodels.api as sm
 num = 100
 x1 = np.linspace(0, 50, num)
 x2 = np.linspace(50, 100, num)
-e = np.ones(100)
 a = np.array([x1, x2])
 X_CL = pd.DataFrame({'coe1': x1, 'coe2': x2})
 X_CL = sm.add_constant(X_CL)
