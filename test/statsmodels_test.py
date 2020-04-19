5be48fabd08ff0edc17bbdd1f0525aa07d3e6e00
gao <37475036@qq.com>
2020-04-19 22:03:19 +0800

    pandas 作为入参回归

--- test/statsmodels_test.py
+++ test/statsmodels_test.py
@@ -7,13 +7,16 @@
 # @Project : AmazingQuant
 # ------------------------------
 import numpy as np
+import pandas as pd
 import statsmodels.api as sm
 num = 100
-x = np.linspace(0, 50, num)
-x1 = np.linspace(50, 100, num)
-a = np.array([x, x1])
-X = sm.add_constant(a)
-y = np.linspace(50, 100, 2)
-model = sm.OLS(X, y)
+x1 = np.linspace(0, 50, num)
+x2 = np.linspace(50, 100, num)
+e = np.ones(2)
+a = np.array([x1, x2])
+X_CL = pd.DataFrame({'coe1': x1, 'coe2': x2})
+X_CL = sm.add_constant(X_CL)
+y = np.linspace(50, 100, num)
+model = sm.OLS(y, X_CL)
 results = model.fit()
 
