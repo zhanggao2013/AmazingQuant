46cfe4a9004d000441c756499a31a3792d151de1
gao <37475036@qq.com>
2020-05-05 13:03:14 +0800

    direct模式, 可订阅单只股票的模式 topic 模式, 可作为全市场订阅的方式

--- test/rabbitmq/rabbitmq_receiver_test.py
+++ test/rabbitmq/rabbitmq_receiver_test.py
@@ -6,7 +6,7 @@
 # @File    : rabbitmq_receiver_test.py
 # @Project : AmazingQuant
 # ------------------------------
-
+import time
 
 import pika
 
@@ -16,16 +16,27 @@ connection = pika.BlockingConnection(parameters)
 
 # 创建通道
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+# channel.queue_declare(queue='hello-1')
+channel.exchange_declare(exchange='direct1-logs',
+                         exchange_type='direct')
+
+result = channel.queue_declare(exclusive=True, queue='')
+queue_name = result.method.queue
+print(queue_name)
+channel.queue_bind(exchange='direct-logs',
+                   queue=queue_name,
+                   routing_key='hello-1')
 
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % (body,))
+    print(" [x] %r:%r" % (method.routing_key, body))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 # 告诉rabbitmq使用callback来接收信息
-# channel.basic_consume(callback, queue='hello', no_ack=True)
-channel.basic_consume("hello", callback, consumer_tag="hello-consumer")
+channel.basic_consume(queue_name, callback, consumer_tag="hello-consumer")
 # 开始接收信息，并进入阻塞状态，队列里有信息才会调用callback进行处理,按ctrl+c退出
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- test/rabbitmq/rabbitmq_send_test.py
+++ test/rabbitmq/rabbitmq_send_test.py
@@ -18,12 +18,14 @@ connection = pika.BlockingConnection(parameters)
 # 创建通道
 channel = connection.channel()
 # 声明一个队列，生产者和消费者都要声明一个相同的队列，用来防止万一某一方挂了，另一方能正常运行
-channel.queue_declare(queue='hello')
+# channel.queue_declare(queue='hello')
 
+channel.exchange_declare(exchange='direct-logs',
+                         exchange_type='direct')
 number = random.randint(1, 1000)
 body = 'hello world:%s' % number
 # 交换机; 队列名,写明将消息发往哪个队列; 消息内容
 # routing_key在使用匿名交换机的时候才需要指定，表示发送到哪个队列
-channel.basic_publish(exchange='', routing_key='hello', body=body)
+channel.basic_publish(exchange='direct-logs', routing_key='hello-1', body=body)
 print(" [x] Sent %s" % body)
 connection.close()
