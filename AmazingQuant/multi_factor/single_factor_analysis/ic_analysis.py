d408bd971431d7d2faa40f3245c2db6f16f9f181
gao <37475036@qq.com>
2020-05-31 00:21:35 +0800

    ic时间序列用stats计算，有p value

--- AmazingQuant/multi_factor/single_factor_analysis/ic_analysis.py
+++ AmazingQuant/multi_factor/single_factor_analysis/ic_analysis.py
@@ -22,6 +22,7 @@ IC是指因子在第T期的因子值与T+1期的股票收益的相关系数
 
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
 from AmazingQuant.constant import LocalDataFolderName, RightsAdjustment
 from AmazingQuant.config.local_data_path import LocalDataPath
@@ -36,17 +37,28 @@ class IcAnalysis(object):
             GetKlineData().cache_all_stock_data(dividend_type=RightsAdjustment.BACKWARD.value, field=['close'])['close']
         self.stock_return = market_data.pct_change().reindex(factor.index).reindex(factor.columns, axis=1)
         self.ic_series = pd.Series({self.factor.index[0]: np.nan})
+        self.p_value_series = pd.Series({self.factor.index[0]: np.nan})
 
-    def cal_ic_series(self, method='pearson'):
+    def cal_ic_series(self, method='spearmanr'):
         """
-        method = {‘pearson’, ‘kendall’, ‘spearman’}
+        method = {‘pearsonr’, ‘spearmanr’}
         :param method:
         :return:
         """
         index_num = 0
         for index, data in self.stock_return.iterrows():
-            if index_num > 0:
-                self.ic_series[index] = data.corr(self.factor.iloc[index_num-1], method=method)
+            if index_num >= 1:
+                stock_list = list(set(data.dropna().index).
+                                  intersection(set(self.factor.iloc[index_num - 1].dropna().index))
+                                  )
+                corr = np.nan
+                p_value = np.nan
+                if method == 'spearmanr':
+                    corr, p_value = stats.spearmanr(data[stock_list], self.factor.iloc[index_num - 1][stock_list])
+                elif method == 'pearsonr':
+                    corr, p_value = stats.pearsonr(data[stock_list], self.factor.iloc[index_num - 1][stock_list])
+                self.ic_series[index] = corr
+                self.p_value_series[index] = p_value
             index_num += 1
         return self.ic_series
 
@@ -56,4 +68,4 @@ if __name__ == '__main__':
     factor_ma5 = get_local_data(path, 'factor_ma5.h5')
 
     ic_analysis_obj = IcAnalysis(factor_ma5)
-    ic_series = ic_analysis_obj.cal_ic_series(method='spearman')
+    ic_series = ic_analysis_obj.cal_ic_series(method='spearmanr')
