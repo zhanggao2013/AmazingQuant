b15164933a304221c1340d9c456b5cbf2504f58b
gao <37475036@qq.com>
2020-05-31 14:28:40 +0800

    ic_delay计算完成

--- AmazingQuant/multi_factor/single_factor_analysis/ic_analysis.py
+++ AmazingQuant/multi_factor/single_factor_analysis/ic_analysis.py
@@ -16,8 +16,8 @@ IC是指因子在第T期的因子值与T+1期的股票收益的相关系数
     （3） IC_IR比率
     （4） IC>0占比
     （5） |IC|>0.02占比(绝对值)
-    （6） p-value（有效性）,全部都计算时间序列
-    （7） IC信号衰减计算
+    （6） p-value（有效性）
+    （7） IC信号衰减计算,全部都计算时间序列
 """
 
 import pandas as pd
@@ -31,13 +31,25 @@ from AmazingQuant.data_center.api_data.get_kline import GetKlineData
 
 
 class IcAnalysis(object):
-    def __init__(self, factor):
+    def __init__(self, factor, factor_name):
         self.factor = factor
-        market_data = \
-            GetKlineData().cache_all_stock_data(dividend_type=RightsAdjustment.BACKWARD.value, field=['close'])['close']
-        self.stock_return = market_data.pct_change().reindex(factor.index).reindex(factor.columns, axis=1)
-        self.ic_series = pd.Series({self.factor.index[0]: np.nan})
-        self.p_value_series = pd.Series({self.factor.index[0]: np.nan})
+        self.factor_name = factor_name
+        market_data = GetKlineData() \
+            .cache_all_stock_data(dividend_type=RightsAdjustment.BACKWARD.value, field=['close'])['close'] \
+            .reindex(factor.index) \
+            .reindex(factor.columns, axis=1)
+
+        self.ic_decay = 2
+        self.stock_return_dict = {i + 1: market_data.pct_change(periods=i + 1) for i in range(self.ic_decay)}
+
+        self.ic_df = pd.DataFrame(columns=[factor_name + '_' + str(i + 1) for i in range(self.ic_decay)])
+        self.p_value_df = pd.DataFrame(columns=[factor_name + '_' + str(i + 1) for i in range(self.ic_decay)])
+
+        self.ic_mean = None
+        self.ic_std = None
+        self.ic_ir = None
+        self.ic_ratio = None
+        self.ic_abs_ratio = None
 
     def cal_ic_series(self, method='spearmanr'):
         """
@@ -45,27 +57,36 @@ class IcAnalysis(object):
         :param method:
         :return:
         """
-        index_num = 0
-        for index, data in self.stock_return.iterrows():
-            if index_num >= 1:
-                stock_list = list(set(data.dropna().index).
-                                  intersection(set(self.factor.iloc[index_num - 1].dropna().index))
-                                  )
+        for index in range(self.factor.shape[0]):
+            print(self.factor.index[index])
+            ic_dict = {}
+            p_value_dict = {}
+            for ic_decay in range(self.ic_decay):
                 corr = np.nan
                 p_value = np.nan
-                if method == 'spearmanr':
-                    corr, p_value = stats.spearmanr(data[stock_list], self.factor.iloc[index_num - 1][stock_list])
-                elif method == 'pearsonr':
-                    corr, p_value = stats.pearsonr(data[stock_list], self.factor.iloc[index_num - 1][stock_list])
-                self.ic_series[index] = corr
-                self.p_value_series[index] = p_value
-            index_num += 1
-        return self.ic_series
+                if index > ic_decay:
+                    stock_return = self.stock_return_dict[ic_decay+1].iloc[index].dropna()
+                    factor_data = self.factor.iloc[index - ic_decay - 1].dropna()
+                    stock_list = list(set(stock_return.index).intersection(set(factor_data.index)))
+                    if method == 'spearmanr':
+                        corr, p_value = stats.spearmanr(stock_return[stock_list], factor_data[stock_list])
+                    elif method == 'pearsonr':
+                        corr, p_value = stats.pearsonr(stock_return[stock_list], factor_data[stock_list])
+                ic_dict[self.factor_name + '_' + str(ic_decay + 1)] = corr
+                p_value_dict[self.factor_name + '_' + str(ic_decay + 1)] = p_value
+
+            self.ic_df = self.ic_df.append(pd.Series(ic_dict, name=self.factor.index[index]))
+            self.p_value_df = self.p_value_df.append(pd.Series(p_value_dict, name=self.factor.index[index]))
+        return self.ic_df, self.p_value_df
+
+    def cal_ic_indicator(self):
+        self.ic_mean = self.ic_df.mean
 
 
 if __name__ == '__main__':
     path = LocalDataPath.path + LocalDataFolderName.FACTOR.value + '/'
     factor_ma5 = get_local_data(path, 'factor_ma5.h5')
 
-    ic_analysis_obj = IcAnalysis(factor_ma5)
-    ic_series = ic_analysis_obj.cal_ic_series(method='spearmanr')
+    ic_analysis_obj = IcAnalysis(factor_ma5, 'factor_ma5')
+    ic_analysis_obj.cal_ic_series(method='spearmanr')
+    ic_analysis_obj.cal_ic_indicator()
