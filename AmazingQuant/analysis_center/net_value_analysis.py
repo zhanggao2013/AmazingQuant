b51e763e0162c6864d4cee0e214d89e2ac8085a9
gao <37475036@qq.com>
2020-06-14 18:46:03 +0800

    净值类指标完成一半

--- AmazingQuant/analysis_center/net_value_analysis.py
+++ AmazingQuant/analysis_center/net_value_analysis.py
@@ -21,9 +21,8 @@ class CalBullBear(object):
     class :CalBullBear
     """
 
-    def __init__(self, index_close, trade_date):
+    def __init__(self, index_close):
         self.index_close = index_close
-        self.timetag_list = list(trade_date)
         self.bull_bear = np.array([0])
 
     def refx(self, data, n):
@@ -87,20 +86,87 @@ class CalBullBear(object):
                 # print("延续前值", bull_bear[-1], index)
             index += 1
         # bull-0  bear-1
-        return
+        return self.bull_bear
 
 
 class NetValueAnalysis(object):
     def __init__(self, net_value_df, benchmark_df, start_time, end_time):
         """
         net_value_df和benchmark_df的index必须在【start_time, end_time】
-        :param net_value_df:
-        :param benchmark_df:
+        :param net_value_df:  columns=['total_balance', 'available', 'net_value', 'profit_ratio', 'drawdown']
+        :param benchmark_df: columns=['close', 'net_value', 'profit_ratio', 'drawdown']
         :param start_time:
         :param end_time:
         """
+        # 净值'net_value', 日收益率'profit_ratio', 总资产'total_balance', 可用资金'available', 最大回撤'drawdown'
         self.net_value_df = net_value_df.loc[start_time: end_time]
+        # 基准收盘价'close', 基准净值'net_value', 基准收益率'profit_ratio', 最大回撤'drawdown'
         self.benchmark_df = benchmark_df.loc[start_time: end_time]
+
+        # 截面数据指标
+        # 年化收益率
+        self.net_year_yield = None
+
+        # 基准年化收益率
+        self.benchmark_year_yield = None
+
+        # 年化波动率
+        self.year_volatility = None
+
+        # 基准年化波动率
+        self.benchmark_year_volatility = None
+
+        # beta
+        self.beta = None
+
+        # alpha
+        self.alpha = None
+
+        # sharpe
+        self.sharp = None
+
+        # 历史最大回撤
+        self.max_drawdown = None
+
+        # 历史最大回撤结束时间
+        self.max_drawdown_end_date = None
+
+        # 历史最大回撤开始时间
+        self.max_drawdown_start_date = None
+
+        # 上涨区间胜率
+        self.bull_win_index_ratio = None
+
+        # 下跌区间胜率
+        self.bear_win_index_ratio = None
+
+        # 日收益率分布
+        self.day_ratio_distribution = {"-10%以下": 0, "-10%~-5%": 0, "-5%~-3%": 0, "-3%~-2%": 0, "-2%~-1%": 0,
+                                       "-1%~0%": 0,
+                                       "0%~1%": 0, "1%~2%": 0, "2%~3%": 0, "3%~5%": 0, "5%~10%": 0, "10%以上": 0}
+
+        # 日均收益率
+        self.day_ratio_average = None
+
+        # 月收益率
+        self.month_ratio = {}
+
+        # 月均收益率
+        self.month_ratio_average = None
+
+        # 日收益波动率
+        self.day_volatility = None
+
+        # 月收益率波动率
+        self.month_volatility = None
+        # HM模型
+        self.HM_model = {'selection': None, 'timing': None, 'p_value': None, }
+
+        # TM模型
+        self.TM_model = {'selection': None, 'timing': None, 'p_value': None, }
+
+        # HM模型
+        self.CL_model = {'selection': None, 'timing': None, 'p_value': None, }
 
     def cal_net_value(self):
         self.net_value_df.insert(loc=0, column='net_value',
@@ -112,161 +178,82 @@ class NetValueAnalysis(object):
         self.net_value_df.insert(loc=0, column='profit_ratio', value=self.net_value_df['net_value'].pct_change() * 100)
         self.benchmark_df.insert(loc=0, column='profit_ratio', value=self.benchmark_df['net_value'].pct_change() * 100)
 
+    def cal_drawdown(self):
+        net_drawdown = (self.net_value_df['net_value']
+                        .div(self.net_value_df['net_value'].expanding().max()) - 1) * 100
+        self.net_value_df.insert(loc=0, column='drawdown', value=net_drawdown)
 
+        benchmark_drawdown = (self.benchmark_df['net_value']
+                              .div(self.benchmark_df['net_value'].expanding().max()) - 1) * 100
+        self.benchmark_df.insert(loc=0, column='drawdown', value=benchmark_drawdown)
 
-
-
-class NetValueIndicator(object):
-    """
-    class:净值类指标计算
-    """
-
-    def __init__(self, net_value, index_close, trade_date, backtesting_day_num=252):
-        """
-        Parameters
-        ----------
-        net_value:np.array
-            净值
-        index_close:np.array
-            基准净值
-        trade_date:np.array
-            交易日列表
-        backtesting_day_num:int
-            计算最近多少个交易日
-        """
-        self.backtesting_day_num = backtesting_day_num
-        self.net_value = net_value[-self.backtesting_day_num:]
-        self.index_close = index_close[-self.backtesting_day_num:]
-        self.trade_date = trade_date[-self.backtesting_day_num:]
-        self.bull_bear = CalBullBear(self.index_close, self.trade_date).get_index_bull_bear()
-
-        self.net_value_ratio = np.array([])
-        self.index_close_ratio = np.array([])
-        self.net_year_yield = np.array([])
-        self.index_year_yield = np.array([])
-
-        self.year_volatility_array = np.array([])
-        self.beta_array = np.array([])
-        self.alpha_array = np.array([])
-        self.sharp_array = np.array([])
-        self.drawdown_array = np.array([])
-        self.max_drawdown_array = np.array([])
-        self.max_drawdown_end_date = ""
-        self.max_drawdown_start_date = ""
-        self.bull_win_index_ratio = 0
-        self.bear_win_index_ratio = 0
-        self.day_ratio_distribution = {"-10%以下": 0, "-10%~-5%": 0, "-5%~-3%": 0, "-3%~-2%": 0, "-2%~-1%": 0,
-                                       "-1%~0%": 0,
-                                       "0%~1%": 0, "1%~2%": 0, "2%~3%": 0, "3%~5%": 0, "5%~10%": 0, "10%以上": 0}
-        self.day_ratio_average = 0
-        self.month_ratio = {}
-        self.month_ratio_average = 0
-        self.day_volatility = 0
-        self.month_volatility = 0
-        self.selection_ability = 0
-        self.timing_ability = 0
-
-    def get_net_value(self, close):
-        return np.array([close[i] / close[0] for i in range(len(close))])
-
-    def get_profit_ratio(self, close):
-        return np.insert(100 * (close[1:] / close[:-1] - 1), 0, 0)
-
-    def get_year_volatility(self):
-        for index in range(len(self.net_value_ratio)):
-            if index > 0:
-                volatility = math.sqrt(252) * np.std(self.net_value_ratio[:index + 1])
-            else:
-                volatility = 0
-            self.year_volatility_array = np.append(self.year_volatility_array, volatility)
-        return self.year_volatility_array
-
-    def get_year_yield(self, close):
-        return np.array([100 * (pow(close[index], 252.0 / (index + 1)) - 1) for index in range(len(close))])
-
-    def get_beta(self):
-        for i in range(len(self.index_close)):
-            if i > 0:
-                beta = (np.cov(self.net_value_ratio, self.index_close_ratio)[0, 1]) / np.var(self.index_close_ratio)
-            else:
-                beta = 0
-            self.beta_array = np.append(self.beta_array, beta)
-        return self.beta_array
-
-    def get_alpha(self):
-        for i in range(len(self.index_year_yield)):
-            alpha = self.net_year_yield[i] - (3.0 + self.beta_array[i] * (self.index_year_yield[i] - 3.0))
-            self.alpha_array = np.append(self.alpha_array, alpha)
-        return self.alpha_array
-
-    def get_sharp(self):
-        for index in range(len(self.net_year_yield)):
-            if self.year_volatility_array[index] > 0:
-                sharp = (self.net_year_yield[index] - 3.0) / self.year_volatility_array[index]
-            else:
-                sharp = 0
-            self.sharp_array = np.append(self.sharp_array, sharp)
-        return self.sharp_array
-
-    def get_max_drawdown(self):
-        for timetag_index in range(len(self.net_value)):
-            if timetag_index > 0:
-                drawdown = 1 - self.net_value[timetag_index] / max(self.net_value[:timetag_index + 1])
-            else:
-                drawdown = 0
-            self.drawdown_array = np.append(self.drawdown_array, drawdown)
-        for timetag_index in range(len(self.drawdown_array)):
-            if timetag_index > 0:
-                max_drawdown = 100 * max(self.drawdown_array[:timetag_index + 1])
-            else:
-                max_drawdown = 0
-            self.max_drawdown_array = np.append(self.max_drawdown_array, max_drawdown)
-        max_drawdown_end_index = np.argmax(self.drawdown_array)
-        max_drawdown_start_index = 0
-        for i in range(len(self.drawdown_array)):
-            if self.drawdown_array[i] == 0 and i < max_drawdown_end_index:
-                max_drawdown_start_index = i
-        self.max_drawdown_end_date = self.trade_date[np.argmax(self.drawdown_array)]
-        self.max_drawdown_start_date = self.trade_date[max_drawdown_start_index]
-        return self.drawdown_array, self.max_drawdown_array, self.max_drawdown_start_date, self.max_drawdown_end_date
-
-    def get_win_index_ratio(self):
+    def cal_win_index_ratio(self):
+        bull_bear = CalBullBear(self.benchmark_df['close'].values).get_index_bull_bear()
         index = 0
         bull_win_num = 0
         bear_win_num = 0
-        for x in np.nditer(self.bull_bear):
-            if x == 0 and self.net_value_ratio[index] > self.index_close_ratio[index]:
+        for x in np.nditer(bull_bear):
+            if x == 0 and self.net_value_df['profit_ratio'].iloc[index] > self.benchmark_df['profit_ratio'].iloc[index]:
                 bull_win_num += 1
-            if x == 1 and self.net_value_ratio[index] > self.index_close_ratio[index]:
+            if x == 1 and self.net_value_df['profit_ratio'].iloc[index] > self.benchmark_df['profit_ratio'].iloc[index]:
                 bear_win_num += 1
             index += 1
-        self.bull_win_index_ratio = bull_win_num / np.sum(self.bull_bear) * 100
-        self.bear_win_index_ratio = bear_win_num / (self.backtesting_day_num - np.sum(self.bull_bear)) * 100
+        self.bull_win_index_ratio = bull_win_num / (self.net_value_df.shape[0] - np.sum(bull_bear)) * 100
+        self.bear_win_index_ratio = bear_win_num / np.sum(bull_bear) * 100
         return self.bull_win_index_ratio, self.bear_win_index_ratio
 
-    def get_day_win_ratio(self):
-        return np.sum(self.net_value_ratio >= 0) / self.backtesting_day_num
+    @staticmethod
+    def cal_year_yield(net_value_series):
+        return 100 * (pow(net_value_series.iloc[-1], 245.0 / (len(net_value_series) + 1)) - 1)
 
-    def get_day_ratio_distribution(self):
-        self.day_ratio_distribution = {"-10%以下": np.sum(self.net_value_ratio < -10),
-                                       "-10%~-5%": np.sum((self.net_value_ratio >= -10) & (self.net_value_ratio < -5)),
-                                       "-5%~-3%": np.sum((self.net_value_ratio >= -5) & (self.net_value_ratio < -3)),
-                                       "-3%~-2%": np.sum((self.net_value_ratio >= -3) & (self.net_value_ratio < -2)),
-                                       "-2%~-1%": np.sum((self.net_value_ratio >= -2) & (self.net_value_ratio < -1)),
-                                       "-1%~0%": np.sum((self.net_value_ratio >= -1) & (self.net_value_ratio < 0)),
-                                       "0%~1%": np.sum((self.net_value_ratio >= 0) & (self.net_value_ratio < 1)),
-                                       "1%~2%": np.sum((self.net_value_ratio >= 1) & (self.net_value_ratio < 2)),
-                                       "2%~3%": np.sum((self.net_value_ratio >= 2) & (self.net_value_ratio < 3)),
-                                       "3%~5%": np.sum((self.net_value_ratio >= 3) & (self.net_value_ratio < 5)),
-                                       "5%~10%": np.sum((self.net_value_ratio >= 5) & (self.net_value_ratio < 10)),
-                                       "10%以上": np.sum(self.net_value_ratio >= 10), }
-        self.day_ratio_distribution = {key: 100 * value / self.backtesting_day_num for key, value in
-                                       self.day_ratio_distribution.items()}
+    @staticmethod
+    def cal_year_volatility(profit_ratio):
+        return math.sqrt(245.0) * profit_ratio.std()
 
-    def get_day_ratio_average(self):
-        self.day_ratio_average = 100 * np.mean(self.net_value_ratio)
+    @staticmethod
+    def cal_beta(net_profit_ratio, benchmark_profit_ratio):
+        return (np.cov(net_profit_ratio.values[1:], benchmark_profit_ratio.values[1:])[0, 1]) / \
+               np.var(benchmark_profit_ratio)
 
-    def get_month_ratio(self):
+    @staticmethod
+    def cal_alpha(net_year_yield, benchmark_year_yield, beta):
+        return net_year_yield - (3.0 + beta * (benchmark_year_yield - 3.0))
+
+    @staticmethod
+    def cal_sharpe(net_year_yield, net_year_volatility):
+        return (net_year_yield - 3.0) / net_year_volatility
+
+    @staticmethod
+    def cal_max_drawdown(drawdown_series):
+        return drawdown_series.min()
+
+    @staticmethod
+    def cal_day_win_ratio(profit_ratio):
+        return np.sum(profit_ratio >= 0) / len(profit_ratio)
+
+    @staticmethod
+    def cal_day_ratio_distribution(profit_ratio):
+        day_ratio_distribution = {"-10%以下": np.sum(profit_ratio < -10),
+                                  "-10%~-5%": np.sum((profit_ratio >= -10) & (profit_ratio < -5)),
+                                  "-5%~-3%": np.sum((profit_ratio >= -5) & (profit_ratio < -3)),
+                                  "-3%~-2%": np.sum((profit_ratio >= -3) & (profit_ratio < -2)),
+                                  "-2%~-1%": np.sum((profit_ratio >= -2) & (profit_ratio < -1)),
+                                  "-1%~0%": np.sum((profit_ratio >= -1) & (profit_ratio < 0)),
+                                  "0%~1%": np.sum((profit_ratio >= 0) & (profit_ratio < 1)),
+                                  "1%~2%": np.sum((profit_ratio >= 1) & (profit_ratio < 2)),
+                                  "2%~3%": np.sum((profit_ratio >= 2) & (profit_ratio < 3)),
+                                  "3%~5%": np.sum((profit_ratio >= 3) & (profit_ratio < 5)),
+                                  "5%~10%": np.sum((profit_ratio >= 5) & (profit_ratio < 10)),
+                                  "10%以上": np.sum(profit_ratio >= 10), }
+        day_ratio_distribution = {key: value / len(profit_ratio) for key, value in day_ratio_distribution.items()}
+        return day_ratio_distribution
+
+    @staticmethod
+    def cal_day_ratio_average(profit_ratio):
+        return profit_ratio.mean()
+
+    @staticmethod
+    def cal_month_ratio(net_value):
         for i in range(len(self.trade_date) - 1):
             if self.trade_date[i][:-2] in self.month_ratio.keys():
                 self.month_ratio[self.trade_date[i][:-2]].append(self.net_value[i])
@@ -274,68 +261,10 @@ class NetValueIndicator(object):
                 self.month_ratio[self.trade_date[i][:-2]] = [self.net_value[i]]
         self.month_ratio = {key: 100 * (value[-1] / value[0] - 1) for key, value in self.month_ratio.items()}
 
-    def get_month_ratio_average(self):
-        self.month_ratio_average = sum(self.month_ratio.values()) / len(self.month_ratio.values())
-
-    def get_day_volatility(self):
-        self.day_volatility = np.std(self.net_value_ratio)
-
-    def get_month_volatility(self):
-        self.month_volatility = np.std(list(self.month_ratio.values()))
-
-    def get_selection_timing(self):
-        self.selection_ability = 0
-        self.timing_ability = 0
-        rf = 0.03 / 252
-        excess_rate = self.net_value_ratio - rf
-        coe_beta1 = self.index_close_ratio - rf
-
-        coe_beta2_HM = np.multiply(coe_beta1, coe_beta1 > 0)
-        X_HM = pd.DataFrame({'coe1': coe_beta1, 'coe2': coe_beta2_HM})
-        X_HM = sm.add_constant(X_HM)
-        estimate_HM = sm.OLS(excess_rate, X_HM).fit()
-        print("HM模型结果", estimate_HM.params, estimate_HM.pvalues, estimate_HM.tvalues, estimate_HM.rsquared)
-
-        coe_beta2_TM = coe_beta1 ** 2
-        X_TM = pd.DataFrame({'coe1': coe_beta1, 'coe2': coe_beta2_TM})
-        X_TM = sm.add_constant(X_TM)
-        estimate_TM = sm.OLS(excess_rate, X_TM).fit()
-        print("TM模型结果", estimate_TM.params, estimate_TM.pvalues, estimate_TM.tvalues, estimate_TM.rsquared)
-
-        coe_beta1_CL = np.multiply(coe_beta1, coe_beta1 > 0)
-        coe_beta2_CL = np.multiply(coe_beta1, coe_beta1 < 0)
-        X_CL = pd.DataFrame({'coe1': coe_beta1_CL, 'coe2': coe_beta2_CL})
-        X_CL = sm.add_constant(X_CL)
-        estimate_CL = sm.OLS(excess_rate, X_CL).fit()
-        print("CL模型结果", estimate_CL.params, estimate_CL.pvalues, estimate_CL.tvalues, estimate_CL.rsquared)
-
-    def process(self):
-        self.net_value = self.get_net_value(self.net_value)
-        self.index_close = self.get_net_value(self.index_close)
-        self.net_value_ratio = self.get_profit_ratio(self.net_value)
-        self.index_close_ratio = self.get_profit_ratio(self.index_close)
-        self.net_year_yield = self.get_year_yield(self.net_value)
-        self.index_year_yield = self.get_year_yield(self.index_close)
-
-        self.get_year_volatility()
-        self.get_beta()
-        self.get_alpha()
-        self.get_sharp()
-        self.get_max_drawdown()
-        self.get_win_index_ratio()
-        self.get_day_win_ratio()
-        self.get_day_ratio_distribution()
-        self.get_day_ratio_average()
-        self.get_month_ratio()
-        self.get_month_ratio_average()
-        self.get_day_volatility()
-        self.get_month_volatility()
-        self.get_selection_timing()
-
 
 if __name__ == '__main__':
-    start_time = datetime(2010, 1, 4)
-    end_time = datetime(2019, 11, 4)
+    start_time = datetime(2018, 1, 2)
+    end_time = datetime(2018, 12, 28)
     kline_object = GetKlineData()
     #
     # 指数行情，沪深300代替
@@ -353,3 +282,37 @@ if __name__ == '__main__':
     net_value_analysis_obj = NetValueAnalysis(net_value_single_account_df, benchmark_df, start_time, end_time)
     net_value_analysis_obj.cal_net_value()
     net_value_analysis_obj.cal_profit_ratio()
+    net_value_analysis_obj.cal_drawdown()
+
+    net_year_yield = net_value_analysis_obj.cal_year_yield(net_value_analysis_obj.net_value_df['net_value'])
+    benchmark_year_yield = net_value_analysis_obj.cal_year_yield(net_value_analysis_obj.benchmark_df['net_value'])
+
+    net_year_volatility = net_value_analysis_obj.cal_year_volatility(
+        net_value_analysis_obj.net_value_df['profit_ratio'])
+    benchmark_year_volatility = net_value_analysis_obj.cal_year_volatility(
+        net_value_analysis_obj.benchmark_df['profit_ratio'])
+
+    beta = net_value_analysis_obj.cal_beta(net_value_analysis_obj.net_value_df['profit_ratio'],
+                                           net_value_analysis_obj.benchmark_df['profit_ratio'])
+
+    alpha = net_value_analysis_obj.cal_alpha(net_year_yield, benchmark_year_yield, beta)
+    sharpe = net_value_analysis_obj.cal_sharpe(net_year_yield, net_year_volatility)
+
+    net_max_drawdown = net_value_analysis_obj.cal_max_drawdown(net_value_analysis_obj.net_value_df['drawdown'])
+    benchmark_max_drawdown = net_value_analysis_obj.cal_max_drawdown(net_value_analysis_obj.benchmark_df['drawdown'])
+
+    bull_win_index_ratio, bear_win_index_ratio = net_value_analysis_obj.cal_win_index_ratio()
+
+    net_day_win_ratio = net_value_analysis_obj.cal_day_win_ratio(net_value_analysis_obj.net_value_df['profit_ratio'])
+    benchmark_day_win_ratio = net_value_analysis_obj.cal_day_win_ratio(
+        net_value_analysis_obj.benchmark_df['profit_ratio'])
+
+    net_day_ratio_distribution = net_value_analysis_obj.cal_day_ratio_distribution(
+        net_value_analysis_obj.net_value_df['profit_ratio'])
+    benchmark_day_ratio_distribution = net_value_analysis_obj.cal_day_ratio_distribution(
+        net_value_analysis_obj.benchmark_df['profit_ratio'])
+
+    net_day_ratio_average = net_value_analysis_obj.cal_day_ratio_average(
+        net_value_analysis_obj.net_value_df['profit_ratio'])
+    benchmark_day_ratio_average = net_value_analysis_obj.cal_day_ratio_average(
+        net_value_analysis_obj.benchmark_df['profit_ratio'])
