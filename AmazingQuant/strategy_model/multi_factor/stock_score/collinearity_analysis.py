182c132d07ab853a2356a116616d11017e90ea4c
gao <37475036@qq.com>
2020-07-05 13:23:24 +0800

    相关系数分析计算完成

--- AmazingQuant/strategy_model/multi_factor/stock_score/collinearity_analysis.py
+++ AmazingQuant/strategy_model/multi_factor/stock_score/collinearity_analysis.py
@@ -13,6 +13,9 @@
 (3)条件数
 """
 from datetime import datetime
+
+import pandas as pd
+
 from AmazingQuant.constant import LocalDataFolderName
 from AmazingQuant.config.local_data_path import LocalDataPath
 from AmazingQuant.data_center.api_data.get_data import get_local_data
@@ -23,14 +26,24 @@ class CollinearityAnalysis(object):
         """
         :param factor_data: dict, key(str):factor name, value(dataframe):factor data
         """
-        self.factor_data = factor_data
+        self.factor_data = pd.concat(factor_data, names=['factor_name'])
+        self.time_tag_index = self.factor_data.index.levels[1].unique()
+
+        # 相关系数， Dataframe, multiindex: time_tag -- factor name, columns: factor name
+        self.data_relation = None
 
     def cal_data_relation(self):
         """
 
         :return: Dataframe, multiindex: time_tag -- factor name, columns: factor name
         """
-        pass
+        self.data_relation = {}
+        for time_tag in self.time_tag_index:
+            time_tag_data = self.factor_data[self.factor_data.index.get_level_values(1) == time_tag]
+            time_tag_data = time_tag_data.reset_index(level='time_tag', drop=True)
+            print(time_tag)
+            self.data_relation[time_tag] = time_tag_data.T.corr()
+        self.data_relation = pd.concat(self.data_relation, names=['time_tag'])
 
     def cal_vif(self):
         """
@@ -57,3 +70,4 @@ if __name__ == '__main__':
     factor_ma10 = factor_ma10[factor_ma10.index < datetime(2020, 1, 1)]
     factor_data = {'factor_ma5': factor_ma5, 'factor_ma10': factor_ma10}
     collinearity_analysis_obj = CollinearityAnalysis(factor_data)
+    collinearity_analysis_obj.cal_data_relation()
