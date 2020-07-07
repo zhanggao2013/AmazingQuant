d74a812113f431292eef5258d6f94af0f6ad1b3c
zhanggao2013 <82045571@qq.com>
2020-07-07 15:41:18 +0800

    因子共线性分析完成

--- AmazingQuant/strategy_model/multi_factor/stock_score/collinearity_analysis.py
+++ AmazingQuant/strategy_model/multi_factor/stock_score/collinearity_analysis.py
@@ -7,7 +7,6 @@
 # @Project : AmazingQuant
 # ------------------------------
 """
-https://blog.csdn.net/u011517132/article/details/105323135
 因子共线性分析
 (1)相关性分析
 (2)方差膨胀因子VIF
@@ -16,6 +15,8 @@ https://blog.csdn.net/u011517132/article/details/105323135
 from datetime import datetime
 
 import pandas as pd
+import statsmodels.api as sm
+import numpy as np
 
 from AmazingQuant.constant import LocalDataFolderName
 from AmazingQuant.config.local_data_path import LocalDataPath
@@ -29,44 +30,40 @@ class CollinearityAnalysis(object):
         """
         self.factor_data = pd.concat(factor_data, names=['factor_name'])
         self.time_tag_index = self.factor_data.index.levels[1].unique()
-
+        self.factor_name_index = self.factor_data.index.levels[0].unique()
         # 相关系数， Dataframe, multiindex: time_tag -- factor name, columns: factor name
         self.relation = None
-
+        # 方差膨胀因子vif, Dataframe, index: time_tag , columns: factor name
         self.vif = pd.DataFrame(columns=self.factor_data.index.levels[0].unique())
+        # 条件数, Series, index: time_tag
+        self.condition_num = pd.Series(index=self.time_tag_index)
 
-    def cal_relation(self):
-        """
+    def cal_relation(self, time_tag, time_tag_data):
+        if time_tag == self.time_tag_index[0]:
+            self.relation = {}
+        self.relation[time_tag] = time_tag_data.T.corr()
+        if time_tag == self.time_tag_index[-1]:
+            self.relation = pd.concat(self.relation, names=['time_tag'])
 
-        :return: Dataframe, multiindex: time_tag -- factor name, columns: factor name
-        """
-        self.relation = {}
+    def cal_vif(self, time_tag, time_tag_data):
+        vif_dict = {}
+        for factor_name in self.factor_name_index:
+            model = sm.OLS(time_tag_data.loc[factor_name].T,
+                           sm.add_constant(time_tag_data[time_tag_data.index != factor_name].T))
+            fit_result = model.fit()
+            vif_dict[factor_name] = 1 / (1 - fit_result.rsquared)
+        self.vif = self.vif.append(pd.Series(vif_dict, name=time_tag))
+
+    def cal_condition_num(self, time_tag, time_tag_data):
+        self.condition_num[time_tag] = np.linalg.cond(time_tag_data.T.values)
+
+    def cal_collinearity(self):
         for time_tag in self.time_tag_index:
             time_tag_data = self.factor_data[self.factor_data.index.get_level_values(1) == time_tag]
             time_tag_data = time_tag_data.reset_index(level='time_tag', drop=True)
-            print(time_tag)
-            self.relation[time_tag] = time_tag_data.T.corr()
-        self.relation = pd.concat(self.relation, names=['time_tag'])
-
-    def cal_vif(self):
-        """
-
-        :return:Dataframe, index: time_tag , columns: factor name
-        """
-        for time_tag in self.time_tag_index:
-        neutralize_data = sm.add_constant(neutralize_data)
-        model = sm.OLS(factor, neutralize_data)
-        fit_result = model.fit()
-        # 残差作为中性化后的数据
-        return fit_result.resid
-        pass
-
-    def cal_condition_num(self):
-        """
-
-        :return: Series, index: time_tag
-        """
-        pass
+            self.cal_relation(time_tag, time_tag_data)
+            self.cal_vif(time_tag, time_tag_data)
+            self.cal_condition_num(time_tag, time_tag_data)
 
 
 if __name__ == '__main__':
@@ -78,5 +75,7 @@ if __name__ == '__main__':
     # 指数数据不全，需要删一部分因子数据
     factor_ma10 = factor_ma10[factor_ma10.index < datetime(2020, 1, 1)]
     factor_data = {'factor_ma5': factor_ma5, 'factor_ma10': factor_ma10}
+
     collinearity_analysis_obj = CollinearityAnalysis(factor_data)
-    collinearity_analysis_obj.cal_relation()
+    collinearity_analysis_obj.cal_collinearity()
+
