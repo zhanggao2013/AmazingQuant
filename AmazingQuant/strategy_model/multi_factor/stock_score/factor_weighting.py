f56d9f56ef34b39476669200a69c226d028103df
zhanggao2013 <82045571@qq.com>
2020-07-07 15:47:39 +0800

    因子加权开头

--- AmazingQuant/strategy_model/multi_factor/stock_score/factor_weighting.py
+++ AmazingQuant/strategy_model/multi_factor/stock_score/factor_weighting.py
@@ -15,3 +15,23 @@
 (5)最大化IC_IR加权法
 (6)最大化IC加权法
 """
+
+from AmazingQuant.constant import LocalDataFolderName
+from AmazingQuant.config.local_data_path import LocalDataPath
+from AmazingQuant.data_center.api_data.get_data import get_local_data
+
+
+class FactorWeighting(object):
+    def __init__(self, factor_data=None):
+        pass
+
+
+if __name__ == '__main__':
+    path = LocalDataPath.path + LocalDataFolderName.FACTOR.value + '/'
+    factor_ma5 = get_local_data(path, 'factor_ma5.h5')
+    # 指数数据不全，需要删一部分因子数据
+    factor_ma5 = factor_ma5[factor_ma5.index < datetime(2020, 1, 1)]
+    factor_ma10 = get_local_data(path, 'factor_ma10.h5')
+    # 指数数据不全，需要删一部分因子数据
+    factor_ma10 = factor_ma10[factor_ma10.index < datetime(2020, 1, 1)]
+    factor_data = {'factor_ma5': factor_ma5, 'factor_ma10': factor_ma10}
