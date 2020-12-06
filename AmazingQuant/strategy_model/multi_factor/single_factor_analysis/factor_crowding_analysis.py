10bdc6b1d17c376c5ccb874fc481aaa2eceb26c6
gao <37475036@qq.com>
2020-12-06 20:44:25 +0800

    增加requirement文件

---
+++ AmazingQuant/strategy_model/multi_factor/single_factor_analysis/factor_crowding_analysis.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : 2020/12/6
+# @Author  : gao
+# @File    : factor_crowding_analysis.py
+# @Project : AmazingQuant
+# ------------------------------
+"""
+因子拥挤度测算
+（1）估值价差(Valuation Spread)
+（2）配对相关性(Pairwise Correlation)
+（3）长期收益反转(Long-term Return Reversal),
+（4）因子波动率(Factor Volatility)。
+（5）复合拥挤度， 使用等权的方式对于拥挤度指标进行复合
+
+"""
