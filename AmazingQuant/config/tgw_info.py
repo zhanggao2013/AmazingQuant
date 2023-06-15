eba33661b822e766e8c956898762d76058d252f7
zhanggao2013 <82045571@qq.com>
2023-06-15 08:09:56 +0800

    增加tgw数据源

---
+++ AmazingQuant/config/tgw_info.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : 2023/6/15
+# @Author  : gao
+# @File    : tgw_info.py 
+# @Project : AmazingQuant 
+# ------------------------------
+
+
+class TgwConfig(object):
+    host = "***.***.***.***"
+    port = **00
+    username = "****"
+    password = "z******"
