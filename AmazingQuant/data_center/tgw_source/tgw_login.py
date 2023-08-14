9bd04ff861ac6ba7e709764f7d010f8d5c7821ca
zhangdegao_cf <82045571@qq.com>
2023-08-14 11:21:06 +0800

    登录模块增加托管模式

--- AmazingQuant/data_center/tgw_source/tgw_login.py
+++ AmazingQuant/data_center/tgw_source/tgw_login.py
@@ -29,7 +29,7 @@ class TgwLogSpi(tgw.ILogSpi):
         print("logon json : ", data.logon_json)
 
 
-def tgw_login():
+def tgw_login(server_mode=False):
     log_spi = TgwLogSpi()
     tgw.SetLogSpi(log_spi)
 
@@ -39,7 +39,18 @@ def tgw_login():
     cfg.server_port = TgwConfig.port
     cfg.username = TgwConfig.username
     cfg.password = TgwConfig.password
-    success = tgw.Login(cfg, tgw.ApiMode.kInternetMode, './')  # 互联网模式初始化，可指定证书文件地址
+    success = False
+    # success = tgw.Login(cfg, tgw.ApiMode.kInternetMode, './')  # 互联网模式初始化，可指定证书文件地址
+    if server_mode:
+        cfg.coloca_cfg.channel_mode = tgw.ColocatChannelMode.kTCP | tgw.ColocatChannelMode.kQTCP \
+                                      | tgw.ColocatChannelMode.kRTCP  # tcp订阅通道和查询通道初始化
+        cfg.coloca_cfg.qtcp_channel_thread = 2
+        cfg.coloca_cfg.qtcp_max_req_cnt = 1000
+        cfg.coloca_cfg.enable_order_book = tgw.OrderBookType.kNone
+        cfg.coloca_cfg.entry_size = 10
+        cfg.coloca_cfg.order_queue_size = 10
+        cfg.coloca_cfg.order_book_deliver_interval_microsecond = 10000
+        success = tgw.Login(cfg, tgw.ApiMode.kColocationMode, './')  # 托管模式初始化，可指定证书文件地址
     if not success:
         print("login fail")
         exit(0)
