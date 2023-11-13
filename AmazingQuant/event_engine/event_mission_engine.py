c8e7b9e351b577b3b4e43a44059d5b68b5d2eb5c
zhangdegao_cf <82045571@qq.com>
2023-11-13 17:21:54 +0800

    改单线程

--- AmazingQuant/event_engine/event_mission_engine.py
+++ AmazingQuant/event_engine/event_mission_engine.py
@@ -6,40 +6,28 @@
 # @File    : event_mission_engine.py.py
 # @Project : AmazingQuant
 # ------------------------------
-
-
-from AmazingQuant.event_engine.event_engine_base import EventEngineBase
 from AmazingQuant.trade_center.event_order import *
 from AmazingQuant.trade_center.event_risk_management import *
 
 
 def run_mission_engine(strategy):
     """
-    order  和　risk management 两个事件, ,同样使用event_engine,本地计算的engine,重新隔离出一个engine
+    order和　risk management 两个事件, 同样使用event_engine,本地计算的engine,重新隔离出一个engine
     在risk management
     更新　OrderData,的状态：已报
     :param order_data:
     :return:
     """
-    mission_engine = EventEngineBase()
-
     event_order = EventOrder()
-    mission_engine.put(event_order)
-
-    mission_engine.register(EventType.EVENT_ORDER.value, EventOrder.integer_conversion)
-    mission_engine.register(EventType.EVENT_ORDER.value, EventOrder.account_available_check)
-    mission_engine.register(EventType.EVENT_ORDER.value, EventOrder.position_available_volume_check)
+    EventOrder.integer_conversion(event_order)
+    EventOrder.account_available_check(event_order)
+    EventOrder.position_available_volume_check(event_order)
 
     event_risk_management = EventRiskManagement()
     event_risk_management.event_data_dict["strategy"] = strategy
-    mission_engine.put(event_risk_management)
-
-    mission_engine.register(EventType.EVENT_RISK_MANAGEMENT.value, EventRiskManagement.black_namelist_check)
-    mission_engine.register(EventType.EVENT_RISK_MANAGEMENT.value, EventRiskManagement.change_order_status)
-    mission_engine.register(EventType.EVENT_RISK_MANAGEMENT.value, EventRiskManagement.send_order)
-
-    mission_engine.start()
-    mission_engine.stop()
+    EventRiskManagement.black_namelist_check(event_risk_management)
+    EventRiskManagement.change_order_status(event_risk_management)
+    EventRiskManagement.send_order(event_risk_management)
 
 
 if __name__ == '__main__':
