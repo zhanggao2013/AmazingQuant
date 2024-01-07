4bbb57d246617e4fa0887d05fd74408d0f73ddb2
Administrator <000>
2024-01-07 20:36:01 +0800

    00

---
+++ AmazingQuant/factor_center/barra_factor/dividend_yield.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# @Time    : 2024/1/7
+# @Author  : gao
+# @File    : dividend_yield.py
+# @Project : AmazingQuant
+# ------------------------------
